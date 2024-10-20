e9039104a13f38ebb66413f8a3c46a28fe0a8bcd
erditfejzullahu <erditfejzullahu45@gmail.com>
2024-10-20 15:17:33 +0200

    changes in DTO

--- Database/DTOs/UpdateUser.cs
+++ Database/DTOs/UpdateUser.cs
@@ -16,15 +16,13 @@ namespace Database.DTOs
         public string? Username { get; set; }
         public string? Password { get; set; }
         public string? Email { get; set; }
-        public int Age { get; set; }
+        public int? Age { get; set; }
         public string? Role { get; set; }
-        public string? ProfilePictureUrl { get; set; }
 
     }
 
     public class UpdateProfilePic
     {
-        [Required]
         public string Base64Profile { get; set; }
     }
 
